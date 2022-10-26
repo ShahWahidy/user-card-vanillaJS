5c5907b058c92144bf3ee7b69234b2fb13b9a9b6
Shahwahidy <awahidy@mail.sfsu.edu>
2022-10-26 14:45:18 -0700

    minor bug fixes, code polish

--- public/script.js
+++ public/script.js
@@ -1,19 +1,19 @@
 
 fetch("https://jsonplaceholder.typicode.com/users")
     // Converting received data to JSON
-    .then((response) => response.json())
-    .then((json) => {
+    .then((data) => data.json())
+    .then((data) => {
 
         //Creating a variable to store HTML table headers
         let li = `<tr><th>ID</th><th>Name</th><th>User Name</th><th>Posts</th></tr>`;
 
         // Looping through each data and add a table row
-        json.forEach((user) => {
+        data.forEach((user) => {
             li += `<tr>
             <td>${user.id}</td>
             <td>${user.name} </td>
             <td>${user.username}</td>
-            <td><button onclick="loadPosts()">Get Posts</button></td>
+            <td><button data-id="${user.id}" onclick="loadPosts(event)">Get Posts</button></td>
             </tr>`;
         });
 
@@ -22,17 +22,22 @@ fetch("https://jsonplaceholder.typicode.com/users")
     });
 
 // function to load posts after user clicks the button
-function loadPosts() {
-
+function loadPosts(event) {
+    //to delete the preveious posts
     deletePosts();
 
+    // grabing the posts div to inject the new data from api into it
     const postEl = document.getElementById("posts")
+    //grabbing user id
+    let Id = event.target.dataset.id
+    console.log(Id, 'Id from event obj')
 
-    let Id = event.target.dataset.userId
 
-    fetch(`https://jsonplaceholder.typicode.com/posts?${Id}`)
+    //making api call
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${Id}`)
         .then((data) => data.json())
         .then((data) => {
+            // console.log(data)
             for (let { body, title } of data) {
                 const postTitle = document.createElement("Strong")
                 const postBody = document.createElement("p")
